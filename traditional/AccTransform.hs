387061ba8fedd5d2c4ccfecbe69668acb95209d5
David Young <dyoung@ittc.ku.edu>
2016-07-06 13:29:55 -0500

    Progress in boolean transformation

--- traditional/AccTransform.hs
+++ traditional/AccTransform.hs
@@ -22,22 +22,23 @@ install _ todo = do
   return (todo ++ [CoreDoPluginPass "Accelerate transformation" (bindsOnlyPass pass)])
 
 pass :: [CoreBind] -> CoreM [CoreBind]
-pass = mapM transformBind
+pass = mapM (transformBind transformExpr)
 
 
-transformBind :: Bind CoreBndr -> CoreM (Bind CoreBndr)
-transformBind (NonRec b e) = do
-  e' <- transformExpr e
+transformBind :: (Expr CoreBndr -> CoreM (Expr CoreBndr)) -> Bind CoreBndr -> CoreM (Bind CoreBndr)
+transformBind f (NonRec b e) = do
+  e' <- f e
   return $ NonRec b e'
-transformBind (Rec bs) = do
-  e' <- mapM (traverse transformExpr) bs
+transformBind f (Rec bs) = do
+  e' <- mapM (traverse f) bs
   return $ Rec e'
 
 infixl 0 :$
+pattern (:$) :: Expr a -> Arg a -> Expr a
 pattern f :$ x = App f x
 
 transformExpr :: Expr CoreBndr -> CoreM (Expr CoreBndr)
-transformExpr = transformRecs <=< transformBools
+transformExpr = transformRecs <=< transformBools2 <=< transformBools
 
 transformRecs :: Expr CoreBndr -> CoreM (Expr CoreBndr)
 transformRecs = return
@@ -56,18 +57,32 @@ boolReplacements =
   ]
 
 -- TODO: Only replace boolean expressions of the appropriate type
-transformBools :: Expr CoreBndr -> CoreM (Expr CoreBndr)
-transformBools e@(Var v)             = do
+
+applyBoolTransform :: Expr CoreBndr -> CoreM (Maybe (Expr CoreBndr))
+applyBoolTransform e@(Var v) = do
   repls <- boolReplacements
   case lookup (varName v) repls of
-    Just repl -> return . Var $ setVarName v repl
-    Nothing -> return e
+    Just repl -> do
+      return . Just . Var
+             $ setVarName v repl
+    Nothing -> return Nothing
+applyBoolTransform _ = return Nothing
+
+transformBools :: Expr CoreBndr -> CoreM (Expr CoreBndr)
+transformBools e@(Var v)             = return e
 transformBools e@(Lit {})            = return e
+transformBools e@(f :$ Type ty1 :$ dict :$ x :$ y) = do
+  fm' <- applyBoolTransform f
+  case fm' of
+    Just f' ->
+      return (f' :$ Type ty1 :$ x :$ y)
+    Nothing ->
+      return e
 transformBools (App f x)             = App <$> transformBools f <*> transformBools x
 transformBools (Lam v b)             = Lam v <$> transformBools b
-transformBools (Let bnd b)           = Let <$> transformBind bnd <*> transformBools b
-transformBools (Case e wild ty alts) =
-  Case <$> transformBools e
+transformBools (Let bnd b)           = Let <$> transformBind transformBools bnd <*> transformBools b
+transformBools (Case c wild ty alts) =
+  Case <$> transformBools c
        <*> pure wild
        <*> pure ty
        <*> mapM (\(x, y, z) -> do
@@ -78,4 +93,29 @@ transformBools (Cast e co)           = Cast <$> transformBools e <*> pure co
 transformBools (Tick t e)            = Tick t <$> transformBools e
 transformBools e@(Type {})           = return e
 transformBools e@(Coercion {})       = return e
+
+-- | Second boolean transformation pass. Turn cases into 'cond's
+transformBools2 :: Expr CoreBndr -> CoreM (Expr CoreBndr)
+transformBools2 e@(Var {})            = return e
+transformBools2 e@(Lit {})            = return e
+transformBools2 (App f x)             = App <$> transformBools2 f <*> transformBools2 x
+transformBools2 (Lam v b)             = Lam v <$> transformBools2 b
+transformBools2 (Let bnd b)           = Let <$> transformBind transformBools2 bnd <*> transformBools2 b
+transformBools2 e@(Case c wild ty alts) = do
+  b <- isAccBool c
+  if b
+    then undefined
+    else return e
+transformBools2 (Cast e co)           = Cast <$> transformBools2 e <*> pure co
+transformBools2 (Tick t e)            = Tick t <$> transformBools2 e
+transformBools2 e@(Type {})           = return e
+transformBools2 e@(Coercion {})       = return e
+
+isAccBool :: Expr CoreBndr -> CoreM Bool
+isAccBool (Var f :$ x :$ y) = do
+  repls <- fmap (map (\(a, b) -> (b, a))) boolReplacements
+  case lookup (varName f) repls of
+    Just _  -> error "Found!"
+    Nothing -> return False
+isAccBool _ = return False
 
