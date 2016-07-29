7148c5031b02392cd7e1635905001da323af213e
David Young <roboguy06@gmail.com>
2016-07-29 02:21:55 -0500

    Added MonadTrans instance

--- traditional/Coroutine.hs
+++ traditional/Coroutine.hs
@@ -5,21 +5,26 @@ module Coroutine where
 
 import           Control.Monad (liftM, ap)
 
-data CoroutineT a m b r where
-  Lift   :: m r -> CoroutineT a m b r
-  Bind   :: CoroutineT a m b x -> (x -> CoroutineT a m b r) -> CoroutineT a m b r
-  Await  :: a -> CoroutineT a m b b
+import           Control.Monad.Trans
 
-instance Applicative m => Functor (CoroutineT a m b) where fmap = liftM
-instance Applicative m => Applicative (CoroutineT a m b) where
+data CoroutineT a m b r where
+  Lift   :: m r -> CoroutineT a b m r
+  Bind   :: CoroutineT a b m x -> (x -> CoroutineT a b m r) -> CoroutineT a b m r
+  Await  :: a -> CoroutineT a b m b
+
+instance Applicative m => Functor (CoroutineT a b m) where fmap = liftM
+instance Applicative m => Applicative (CoroutineT a b m) where
   pure = return
   (<*>) = ap
 
-instance Applicative m => Monad (CoroutineT a m b) where
+instance Applicative m => Monad (CoroutineT a b m) where
   return = Lift . pure
   (>>=)  = Bind
 
-withProducer :: Monad m => (a -> m b) -> CoroutineT a m b r -> m r
+instance MonadTrans (CoroutineT a b) where
+  lift = Lift
+
+withProducer :: Monad m => (a -> m b) -> CoroutineT a b m r -> m r
 withProducer _        (Lift mr)    = mr
 withProducer producer (Bind mx mf) = withProducer producer mx >>= (withProducer producer . mf)
 withProducer producer (Await arg)  = producer arg
