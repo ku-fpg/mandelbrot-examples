44146b45f62d41d02643bfc0b9767275047be154
David Young <dyoung@ittc.ku.edu>
2016-09-06 13:26:34 -0500

    Added basic worker/wrapper functions

---
+++ traditional/WW.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+import           Data.Array.Accelerate
+
+rep :: Lift Exp a => a -> Exp (Plain a)
+rep = lift
+
+-- | All calls to 'abs' should be gone by the time compilation finishes.
+abs :: Exp a -> a
+abs = error "Internal error: abs called"
+
