36eb1f53c16088a7a52df0fd4e9a292a6819f547
David Young <dyoung@ittc.ku.edu>
2016-09-08 12:09:21 -0500

    Monomorphized types

--- traditional/Mandelbrot.hs
+++ traditional/Mandelbrot.hs
@@ -1,10 +1,10 @@
 {-# OPTIONS_GHC -fplugin AccPlugin.AccTransform -fenable-rewrite-rules #-}
 
-{-# LANGUAGE Strict #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE Strict                 #-}
+{-# LANGUAGE RankNTypes             #-}
+{-# LANGUAGE ScopedTypeVariables    #-}
+{-# LANGUAGE FlexibleContexts       #-}
+{-# LANGUAGE FlexibleInstances      #-}
 {-# LANGUAGE FunctionalDependencies #-}
 
 -- NOTE: To run with plugin:
@@ -25,7 +25,7 @@ import           Data.Array.Accelerate.Interpreter (run)
 
 import           GHC.Exts
 
-maxIterations :: Num a => a
+maxIterations :: Float
 maxIterations = 100
 
 width, height :: Int
@@ -33,33 +33,18 @@ width  = 800
 height = 600
 
 -- Scale x to [-2.5, 1] and y to [-1, 1]
-scaleX, scaleY :: Floating a => a -> a
+scaleX, scaleY :: Float -> Float
 scaleX x = -2.5 + x * ((2.5 + 1) / fromIntegral width)
 scaleY y = -1 + y * ((1 + 1) / fromIntegral height)
 
-class NumericConv a b | a -> b where
-    conv :: a -> b
-
-instance NumericConv Int Float where
-    conv = fromIntegral
-
-instance NumericConv (Exp Int) (Exp Float) where
-    conv = A.fromIntegral
-
 -- NOTE: The Ord instances for Exp types are a bit of a lie (w.r.t. all the
 -- comparison operations).
-pointColor :: forall a b. (NumericConv a b, Integral a, Floating b, Ord b) =>
-                a -> a -> (b, b, b)
-pointColor origX' origY' = go (0, 0, 0)
+pointColor :: Int -> Int -> (Float, Float, Float)
+pointColor origX origY = go (0, 0, 0)
   where
-    origX, origY :: b
-    origX = conv origX'
-    origY = conv origY'
+    (scaledX, scaledY) = (scaleX $ fromIntegral origX, scaleY $ fromIntegral origY)
 
-    scaledX, scaledY :: b
-    (scaledX, scaledY) = (scaleX origX, scaleY origY)
-
-    go :: (b, b, b) -> (b, b, b)
+    go :: (Float, Float, Float) -> (Float, Float, Float)
     go (n, x, y)
       | n >= maxIterations = (0, 0, 1)
       | x*x + y*y >= 4     = ((n/maxIterations), 0, 0)
@@ -101,18 +86,18 @@ interpretResult pixels x y =
     in
     PixelRGBF r g b
 
-{-# RULES
-      "generateImage->Acc"
-      forall (f :: forall a b. (NumericConv a b, Integral a, Floating b, Ord b) => a -> a -> (b, b, b)) w h.
-      generateImage (toRGBF f) w h
-        =
-      genRGBF (interpretResult (run (generate
-                            (constant
-                              (Z :. width :. height))
-                            (indexing2 (inline (f :: Exp Int -> Exp Int -> (Exp Float, Exp Float, Exp Float)))))))
-                    w
-                    h
-  #-}
+-- {-# RULES
+--       "generateImage->Acc"
+--       forall (f :: forall a b. (NumericConv a b, Integral a, Floating b, Ord b) => a -> a -> (b, b, b)) w h.
+--       generateImage (toRGBF f) w h
+--         =
+--       genRGBF (interpretResult (run (generate
+--                             (constant
+--                               (Z :. width :. height))
+--                             (indexing2 (inline (f :: Exp Int -> Exp Int -> (Exp Float, Exp Float, Exp Float)))))))
+--                     w
+--                     h
+--   #-}
 
 -- {-# RULES "fromIntegral->A.fromIntegral"
 --       forall (n :: (Elt a, IsIntegral a, Integral a) => Exp a).
