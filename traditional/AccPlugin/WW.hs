26857c20267f8f3b6a3b644a735a9f39b6a085eb
David Young <dyoung@ittc.ku.edu>
2016-09-14 11:44:37 -0500

    Corrected naming for rep/abs (I think)

--- traditional/AccPlugin/WW.hs
+++ traditional/AccPlugin/WW.hs
@@ -4,10 +4,10 @@ module AccPlugin.WW where
 
 import           Data.Array.Accelerate
 
-rep :: Lift Exp a => a -> Exp (Plain a)
-rep = lift
+abs :: Lift Exp a => a -> Exp (Plain a)
+abs = lift
 
--- | All calls to 'abs' should be gone by the time compilation finishes.
-abs :: Exp a -> a
-abs = error "Internal error: abs called"
+-- | All calls to 'rep' should be gone by the time compilation finishes.
+rep :: Exp a -> a
+rep = error "Internal error: rep called"
 
